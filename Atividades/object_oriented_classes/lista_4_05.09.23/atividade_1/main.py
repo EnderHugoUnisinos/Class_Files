6766575d070f747bcb5ad747f5f7ef732e4cf27b
Ender_Hugo <131411734+EnderHugoUnisinos@users.noreply.github.com>
2023-09-05 21:31:33 -0300

    Update main

--- Atividades/object_oriented_classes/lista_4_05.09.23/atividade_1/main.py
+++ Atividades/object_oriented_classes/lista_4_05.09.23/atividade_1/main.py
@@ -3,10 +3,10 @@ from continente import Continente
 
 class System():
     def iniciarPais(self):
-        pais1 = Pais("A", "A", 10, 50)
-        pais2 = Pais("B", "B", 20, 30)
-        pais3 = Pais("C", "C", 30, 20)
-        pais4 = Pais("D", "D", 40, 70)
+        pais1 = Pais("A", "E", 10, 50)
+        pais2 = Pais("B", "F", 20, 30)
+        pais3 = Pais("C", "G", 30, 20)
+        pais4 = Pais("D", "H", 40, 70)
 
         pais1.inserirFronteira([pais2,pais3,pais4])
         pais2.inserirFronteira([pais1,pais3,pais4])
