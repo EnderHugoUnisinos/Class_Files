c801022c8c6af9820f9ca631a75242368d9cc3dc
Ender_Hugo <131411734+EnderHugoUnisinos@users.noreply.github.com>
2023-09-12 18:17:49 -0300

    Update

--- Atividades/object_oriented_classes/lista_4_05.09.23/atividade_1/main.py
+++ Atividades/object_oriented_classes/lista_4_05.09.23/atividade_1/main.py
@@ -2,24 +2,30 @@ from pais import Pais
 from continente import Continente
 
 class System():
-    def iniciarPais(self):
-        pais1 = Pais("A", "E", 10, 50)
-        pais2 = Pais("B", "F", 20, 30)
-        pais3 = Pais("C", "G", 30, 20)
-        pais4 = Pais("D", "H", 40, 70)
-
+    def programStart(self):
+        pais1 = Pais("A", "A", 10, 50)
+        pais2 = Pais("B", "B", 20, 30)
+        pais3 = Pais("C", "C", 30, 20)
+        pais4 = Pais("D", "D", 40, 70)
+    
         pais1.inserirFronteira([pais2,pais3,pais4])
         pais2.inserirFronteira([pais1,pais3,pais4])
+        pais3.inserirFronteira([pais1,pais2,pais4])
+        pais4.inserirFronteira([pais1,pais2,pais3])
 
         continente = Continente("Polvo", [pais1,pais2,pais3,pais4])
         print("Densidade total: {}".format(continente.densidadeTotal()))
         print("Dimensão total: {}".format(continente.dimensaoTotal()))
         print("Razão territorial: {}".format(continente.razaoTerritorial()))
         
+        paises_comuns = pais1.paisesComuns(pais2)
+        print("Paises Comuns [A e B]:",end="")
+        for i in paises_comuns:
+            print(" {} ".format(i.getNome()),end="")
 
 def main():
     system = System()
-    system.iniciarPais()
+    system.programStart()
 
 if __name__ == "__main__":
     main()
