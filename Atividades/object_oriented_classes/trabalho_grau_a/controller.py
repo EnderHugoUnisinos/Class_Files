16b096a1d83c2471a614ae89cedcdfde2727267b
Ender_Hugo <131411734+EnderHugoUnisinos@users.noreply.github.com>
2023-09-24 17:04:01 -0300

    Update

--- Atividades/object_oriented_classes/trabalho_grau_a/controller.py
+++ Atividades/object_oriented_classes/trabalho_grau_a/controller.py
@@ -1,11 +1,14 @@
 class SystemController:
-    def __init__(self, path) -> None:
-        self.path = path
+    def __init__(self, pousada_path, produto_path, quarto_path, reserva_path) -> None:
+        self.pousada_path = pousada_path
+        self.produto_path = produto_path
+        self.quarto_path = quarto_path
+        self.reserva_path = reserva_path
 
     def controller_salvar_dados(self, pousada):
-        with open(self.path, "w") as file:
+        with open(self.pousada_path, "w") as file:
             file.write()
 
     def controller_carregar_dados(self, pousada):
-        with open(self.path, "r") as file:
+        with open(self.pousada_path, "r") as file:
             strings = file.readlines()
